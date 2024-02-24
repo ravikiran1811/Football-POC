05aea7c03ae416d94c15484903cf1d8e63c5ba0d
divami-praveen <praveen@divami.com>
2024-02-24 19:14:02 +0530

    retainer app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import "./App.scss";
 import Router from "./routes";
 import ThemeProvider from "./themes/ThemeProvider";
-import Header from './components/Header/Header'
-// import ResponsiveAppBar from "./components/Header/TestHeader";
+// import { useState } from "react";
+// import InputField from "./components/InputFeild/InputFeild";
+// import { Numberlist } from "./DummyData/MobileNumberData";
+// import MobileNumberComponent from "./components/MobileNumberComponent/MobileNumberComponent";
 function App() {
+  // const [countryCode, setCountryCode] = useState("");
+  // const [number, setNumber] = useState("");
+  // function fun(a: string) {
+  //   console.log(a, "as the number 111");
+  //   setNumber(a);
+  // }
+  // function fun2(a: string) {
+  //   console.log(a, "as the code");
+  //   setCountryCode(a);
+  // }
   return (
     <>
       <ThemeProvider>
-      {/* <ResponsiveAppBar/> */}
-        <Header/>
-        {/* <Router /> */}
-        
+        <Router />
       </ThemeProvider>
     </>
   );
