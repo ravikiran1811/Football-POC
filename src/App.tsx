d40ad2eb180a769acfc6ad794f99fffef33fda1f
unknown <vinay.dudi@divami.com>
2024-02-25 17:58:39 +0530

    carousel section

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ function App() {
     <>
       <ThemeProvider>
         <Router />
-        <Router />
       </ThemeProvider>
     </>
   );
---
+++ src/DummyData/carouselData.ts
@@ -0,0 +1,85 @@
+import testImage from "../assets/pexels-pixabay-326055.jpg";
+
+export const carouselData = [
+  {
+    id: 1,
+    name: "Local Groups and Clubs",
+    imageLink: testImage,
+    experience:
+      "10 Years Exp.",
+    certification:"AIFF ,NCLS",
+  },
+  {
+    id: 2,
+    name: "Local Groups and Clubs",
+    imageLink: testImage,
+    experience:
+      "10 Years Exp.",
+    certification:"AIFF ,NCLS"
+  },
+  {
+    id: 3,
+    name: "Local Groups and Clubs",
+    imageLink: testImage,
+    experience:
+      "10 Years Exp.",
+    certification:"AIFF ,NCLS"
+  },
+  {
+    id: 4,
+    name: "Local Groups and Clubs",
+    imageLink: testImage,
+    experience:
+      "10 Years Exp.",
+    certification:"AIFF ,NCLS"
+  },
+  {
+    id: 5,
+    name: "Local Groups and Clubs",
+    imageLink: testImage,
+    experience:
+      "10 Years Exp.",
+    certification:"AIFF ,NCLS"
+  },
+  {
+    id: 6,
+    name: "Local Groups and Clubs",
+    imageLink: testImage,
+    experience:
+      "10 Years Exp.",
+    certification:"AIFF ,NCLS"
+  },
+  {
+    id: 7,
+    name: "Local Groups and Clubs",
+    imageLink: testImage,
+    experience:
+      "10 Years Exp.",
+    certification:"AIFF ,NCLS"
+  },
+  {
+    id: 8,
+    name: "Local Groups and Clubs",
+    imageLink: testImage,
+    experience:
+      "10 Years Exp.",
+    certification:"AIFF ,NCLS"
+  },
+  {
+    id: 9,
+    name: "Local Groups and Clubs",
+    imageLink: testImage,
+    experience:
+      "10 Years Exp.",
+    certification:"AIFF ,NCLS"
+  },
+  {
+    id: 10,
+    name: "Local Groups and Clubs",
+    imageLink: testImage,
+    experience:
+      "10 Years Exp.",
+    certification:"AIFF ,NCLS"
+  }
+];
+
