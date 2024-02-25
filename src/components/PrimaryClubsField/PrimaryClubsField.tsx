de9657186ebfad765519f161f57d394fa2b583fb
unknown <ravi.pampana@divami.com>
2024-02-25 10:50:38 +0530

    finally updated

--- src/components/PrimaryClubsField/PrimaryClubsField.tsx
+++ src/components/PrimaryClubsField/PrimaryClubsField.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import HeadingField from "../HeadingField/HeadingField";
 import ShowCard from "../CardSecondary/ShowCard";
 import PrimaryClubsFieldStyles from './PrimaryClubsField.module.scss'
@@ -6,8 +6,9 @@ import PrimaryClubsFieldStyles from './PrimaryClubsField.module.scss'
 const PrimaryClubsField=()=>{
     return(
         <Box className={PrimaryClubsFieldStyles.clubFields}>
-            <Box>
+            <Box className={PrimaryClubsFieldStyles.clubFields__heading}>
             <HeadingField heading="CLUBS" subHeading="POPULAR INDIAN" />
+            <Typography className={PrimaryClubsFieldStyles.clubFields__heading__viewmore}>View Club Details</Typography>
             </Box>
             <Box className={PrimaryClubsFieldStyles.clubFields__cards}>
                 <ShowCard />
