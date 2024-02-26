a01c4a2594a45516da91736166b8cb830ed1f6b8
divami-praveen <praveen@divami.com>
2024-02-26 14:00:20 +0530

    fixes

--- src/components/SummarySection/SummarySection.tsx
+++ src/components/SummarySection/SummarySection.tsx
@@ -1,71 +1,127 @@
 import { Box } from "@mui/material";
 import React from "react";
-import summarySectionStyles from "./summarySection.module.scss"
+import summarySectionStyles from "./summarySection.module.scss";
 import { constants } from "../../constants/stringConstants";
 interface ISummaryProps {
-    Eligibility: any;
-    About: string;
+  Eligibility: any;
+  About: string;
 }
 const SummarySection: React.FC<ISummaryProps> = ({ Eligibility, About }) => {
-    return (
-        <>
-            <Box className={summarySectionStyles.tournaments} id="Summary">
-                <Box className={summarySectionStyles.DuranCupImage}>
-                    <img src="../../../src/assets/Durand_Cup 3.svg" ></img>
-                </Box>
-                <Box className={summarySectionStyles.summary}>
-                    <Box className={summarySectionStyles.eligibleDiv}>
-                        <Box>{constants.tournamentSummary.eligibility}</Box>
-                        <Box className={summarySectionStyles.qualities}>
-                            <Box className={summarySectionStyles.eligibility}>
-                                <Box className={summarySectionStyles.eligibilityCriteria}>
-                                    <Box className={summarySectionStyles.criteria}>{constants.tournamentSummary.age}: </Box>
-                                    <Box>
-                                        <Box className={summarySectionStyles.result}>{Eligibility.age[0]}</Box>
-                                        <Box className={summarySectionStyles.checkCriteria}>{constants.tournamentSummary.age}:<Box  className={summarySectionStyles.result}>{Eligibility.age[1]}</Box></Box>
-                                        <Box className={summarySectionStyles.checkCriteria}>{constants.tournamentSummary.dob}:<Box className={summarySectionStyles.result}>{Eligibility.age[2]}</Box></Box>
-                                    </Box>
-                                </Box>
-                            </Box>
-                            <Box className={summarySectionStyles.eligibility}>
-                                <Box className={summarySectionStyles.eligibilityCriteria}>
-                                    <Box className={summarySectionStyles.criteria}>{constants.tournamentSummary.teamComposition}</Box>
-                                    <Box>
-                                    <Box className={summarySectionStyles.result}>{Eligibility.teamComposition[0]}</Box>
-                                        <Box className={summarySectionStyles.checkCriteria}>{constants.tournamentSummary.squadSize}:<Box className={summarySectionStyles.result}>{Eligibility.teamComposition[1]}</Box></Box>
-                                        <Box className={summarySectionStyles.checkCriteria}>{constants.tournamentSummary.coach}:<Box className={summarySectionStyles.result}>{Eligibility.teamComposition[2]}</Box></Box>
-                                    </Box>
-                                </Box>
-                            </Box>
-                            <Box className={summarySectionStyles.eligibility}>
-                                <Box className={summarySectionStyles.eligibilityCriteria}>
-                                    <Box className={summarySectionStyles.criteria}>{constants.tournamentSummary.gender}:</Box>
-                                    <Box>
-                                    <Box className={summarySectionStyles.result}>{Eligibility.gender[0]}</Box>
-                                        <Box className={summarySectionStyles.checkCriteria}>{constants.tournamentSummary.nationality}:<Box className={summarySectionStyles.result}>{Eligibility.gender[1]}</Box></Box>
-                                        <Box className={summarySectionStyles.checkCriteria}>{constants.tournamentSummary.medicalClearance}:<Box className={summarySectionStyles.result}>{Eligibility.gender[2]}</Box></Box>
-                                    </Box>
-                                </Box>
-                            </Box>
-                            <Box className={summarySectionStyles.eligibility}>
-                                <Box className={summarySectionStyles.eligibilityCriteria}>
-                                    <Box className={summarySectionStyles.criteria}>{constants.tournamentSummary.playerExperience}</Box>
-                                    <Box>
-                                    <Box className={summarySectionStyles.result}>{Eligibility.playerExperience[0]}</Box>
-                                        <Box className={summarySectionStyles.checkCriteria}>{constants.tournamentSummary.skillLevel}:<Box className={summarySectionStyles.result}>{Eligibility.playerExperience[1]}</Box></Box>
-                                        <Box className={summarySectionStyles.checkCriteria}>{constants.tournamentSummary.clubAffiliation}:<Box className={summarySectionStyles.result}>{Eligibility.playerExperience[2]}</Box></Box>
-                                    </Box>
-                                </Box>
-                            </Box>
-                        </Box>
+  return (
+    <>
+      <Box className={summarySectionStyles.tournaments} id="Summary">
+        <Box className={summarySectionStyles.DuranCupImage}>
+          <img src="../../../src/assets/Durand_Cup 3.svg"></img>
+        </Box>
+        <Box className={summarySectionStyles.summary}>
+          <Box className={summarySectionStyles.eligibleDiv}>
+            <Box>{constants.tournamentSummary.eligibility}</Box>
+            <Box className={summarySectionStyles.qualities}>
+              <Box className={summarySectionStyles.eligibility}>
+                <Box className={summarySectionStyles.eligibilityCriteria}>
+                  <Box className={summarySectionStyles.criteria}>
+                    {constants.tournamentSummary.age}:{" "}
+                  </Box>
+                  <Box>
+                    <Box className={summarySectionStyles.result}>
+                      {Eligibility.age[0]}
                     </Box>
-                    <Box>
-                        <Box>{constants.tournamentSummary.about}</Box>
-                        <Box className={summarySectionStyles.aboutSummary}>{About}</Box>
+                    <Box className={summarySectionStyles.checkCriteria}>
+                      {constants.tournamentSummary.age}:
+                      <Box className={summarySectionStyles.result}>
+                        {Eligibility.age[1]}
+                      </Box>
                     </Box>
+                    <Box className={summarySectionStyles.checkCriteria}>
+                      {constants.tournamentSummary.dob}:
+                      <Box className={summarySectionStyles.result}>
+                        {Eligibility.age[2]}
+                      </Box>
+                    </Box>
+                  </Box>
                 </Box>
+              </Box>
+              <Box className={summarySectionStyles.eligibility}>
+                <Box className={summarySectionStyles.eligibilityCriteria}>
+                  <Box className={summarySectionStyles.criteria}>
+                    {constants.tournamentSummary.teamComposition}
+                  </Box>
+                  <Box>
+                    <Box className={summarySectionStyles.result}>
+                      {Eligibility.teamComposition[0]}
+                    </Box>
+                    <Box className={summarySectionStyles.checkCriteria}>
+                      {constants.tournamentSummary.squadSize}:
+                      <Box className={summarySectionStyles.result}>
+                        {Eligibility.teamComposition[1]}
+                      </Box>
+                    </Box>
+                    <Box className={summarySectionStyles.checkCriteria}>
+                      {constants.tournamentSummary.coach}:
+                      <Box className={summarySectionStyles.result}>
+                        {Eligibility.teamComposition[2]}
+                      </Box>
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
+              <Box className={summarySectionStyles.eligibility}>
+                <Box className={summarySectionStyles.eligibilityCriteria}>
+                  <Box className={summarySectionStyles.criteria}>
+                    {constants.tournamentSummary.gender}:
+                  </Box>
+                  <Box>
+                    <Box className={summarySectionStyles.result}>
+                      {Eligibility.gender[0]}
+                    </Box>
+                    <Box className={summarySectionStyles.checkCriteria}>
+                      {constants.tournamentSummary.nationality}:
+                      <Box className={summarySectionStyles.result}>
+                        {Eligibility.gender[1]}
+                      </Box>
+                    </Box>
+                    <Box className={summarySectionStyles.checkCriteria}>
+                      {constants.tournamentSummary.medicalClearance}:
+                      <Box className={summarySectionStyles.result}>
+                        {Eligibility.gender[2]}
+                      </Box>
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
+              <Box className={summarySectionStyles.eligibility}>
+                <Box className={summarySectionStyles.eligibilityCriteria}>
+                  <Box className={summarySectionStyles.criteria}>
+                    {constants.tournamentSummary.playerExperience}
+                  </Box>
+                  <Box>
+                    <Box className={summarySectionStyles.result}>
+                      {Eligibility.playerExperience[0]}
+                    </Box>
+                    <Box className={summarySectionStyles.checkCriteria}>
+                      {constants.tournamentSummary.skillLevel}:
+                      <Box className={summarySectionStyles.result}>
+                        {Eligibility.playerExperience[1]}
+                      </Box>
+                    </Box>
+                    <Box className={summarySectionStyles.checkCriteria}>
+                      {constants.tournamentSummary.clubAffiliation}:
+                      <Box className={summarySectionStyles.result}>
+                        {Eligibility.playerExperience[2]}
+                      </Box>
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
             </Box>
-        </>
-    )
-}
+          </Box>
+          <Box>
+            <Box>{constants.tournamentSummary.about}</Box>
+            <Box className={summarySectionStyles.aboutSummary}>{About}</Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
 export default SummarySection;
--- src/pages/TournamentDetails/TournamentDetails.tsx
+++ src/pages/TournamentDetails/TournamentDetails.tsx
@@ -1,10 +1,19 @@
+import { summaryData } from "../../DummyData/summaryContent";
 import { hallOfFameData } from "../../DummyData/hallOfFameData";
+import { sideNavData } from "../../DummyData/sideNavData";
 import ChampionsSection from "../../components/ChampionsSection/ChampionsSection";
 import HallOfFame from "../../components/HallOfFame/HallOfFame";
+import SideNav from "../../components/SideNavBar/SideNav";
+import SummarySection from "../../components/SummarySection/SummarySection";
 
 const TournamentDetails = () => {
   return (
     <>
+      <SummarySection
+        About={summaryData.DuranCup.About}
+        Eligibility={summaryData.DuranCup.Eligibility}
+      />
+      <SideNav content={sideNavData[0].durandCup} />
       <HallOfFame data={hallOfFameData} />
       <ChampionsSection />
     </>
