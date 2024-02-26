11e7952b1515d9cc7fc8398ced1d241aa672da37
UdaybhaskarTalari <20a91a1252@aec.edu.in>
2024-02-26 09:08:51 +0530

    club details with constants

--- src/constants/stringConstants.ts
+++ src/constants/stringConstants.ts
@@ -3,4 +3,12 @@ export const constants = {
     heading: " a file or Drag and drop here",
     subHeading: " 1-5 images, please! Formats: JPG, PNG, SVG or JPEG.",
   },
+  clubpageDetails:{
+    location:'Location',
+    clublevel:'Club Level',
+    category:'Category',
+    primarycontact:'Primary Contact Name',
+    email:'Email-id',
+    contact:'Contact Number'
+  }
 };
