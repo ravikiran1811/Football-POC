3b97bd674badcb3db210c82aa351eadddace15e7
UdaybhaskarTalari <20a91a1252@aec.edu.in>
2024-02-26 15:29:12 +0530

    tournament page image and details and removing the call to TournamentDetais component

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,8 +3,7 @@ import { Outlet } from "react-router-dom";
 import useCurrentLocation from "../../utils/useCurrentLocation";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import TournamentDetais from "../../components/TournamentDetails/TournamentDetais";
-import {tournamentDetails} from '../../DummyData/tournamentDetails';
+
 const Dashboard = () => {
   // const navigate = useNavigate();
   // const handleLogout = () => {
@@ -18,12 +17,11 @@ const Dashboard = () => {
   return (
     <>
       <div className={DashboardStyles.container}>
-        {/* <Header />
+        <Header />
         <Outlet />
 
-        <Footer /> */}
-        <TournamentDetais tournamentName={tournamentDetails[0].name} tournamentAgeGroup={tournamentDetails[0].age} tournamentLevel={tournamentDetails[0].level}
-        tournamentClubAssociated={tournamentDetails[0].clubAssociated} tournamentPrize={tournamentDetails[0].prize} tournamentUpcomingMatch={tournamentDetails[0].upcomingMatch}/>
+        <Footer />
+       
         {/* <button onClick={handleLogout}>Logout</button> */}
       </div>
     </>
